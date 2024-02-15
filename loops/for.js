d7800e63c81cfd2d239070729997eca0353fed75
Tarra3 <151025105+Tarra3@users.noreply.github.com>
2024-02-15 20:57:41 +0200

    for.srv

--- loops/for.js
+++ loops/for.js
@@ -23,3 +23,18 @@ for (let i = 0; i < 5; i++) {
     console.log(i);
 }
 console.log('END')
+
+/////////////////////////////////////////////////////
+console.clear();
+
+const marks = [10, 2, 8, 4, 6, 10];
+
+let sum = 0;
+
+
+for (let i = 0; i < marks.length; i++) {
+    console.log(i, marks[i]);
+
+    sum += marks[i];
+}
+console.log('SUM:', sum, sum / marks.length);

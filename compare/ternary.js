1e16e6b500deb7ab8e0b1425bdd8c2abc55c7824
Tarra3 <151025105+Tarra3@users.noreply.github.com>
2024-02-15 14:02:50 +0200

    ternary

--- compare/ternary.js
+++ compare/ternary.js
@@ -39,3 +39,40 @@ if (amzius >= limitas) {
 }
 console.log(msg);
 
+
+console.log('nauja------------------');
+const x1 = true ? 0 : 1;
+console.log(x1);
+
+const x2 = false ? 0 : 1;
+console.log(x2);
+
+const x3 = 0 ? 1 : 2;
+console.log(x3);
+
+const x4 = 1 ? 2 : 3;
+console.log(x4);
+
+const x5 = 0 ? 1 : 2 ? 3 : 4;
+console.log(x5);
+
+const x6 = 1 ? 2 : 3 ? 4 : 5;
+console.log(x6);
+
+const x7 = '' ? 0 ? 1 : 2 : 3;
+console.log(x7);
+
+const x8 = 0 ? 1 ? 2 : 3 : 4 ? 5 : 6;
+/* = 0 
+ ?1
+    ?2
+    :3
+:4
+    ?5
+    :6;
+*/
+console.log(x8);
+
+
+
+

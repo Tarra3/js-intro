1bb8169faa3d1e959c21c8d0583dd148b80fa689
Tarra3 <151025105+Tarra3@users.noreply.github.com>
2024-02-21 15:12:11 +0200

    while

---
+++ loops/while.js
@@ -0,0 +1,25 @@
+
+// budas 1: (10-15 eilute)
+for (let i = 10; i <= 15; i++) {
+    console.log(i);
+}
+
+console.log('--------------222-----------');
+
+// budas 2: (10-15 eilute) 
+let i = 10;
+for (; i <= 15;) {
+    console.log(i);
+    i++;
+}
+
+console.log('--------33333333------------');
+
+// budas 3: (10-15 eilute) 
+let k = 10;
+while (k <= 15) {
+    console.log(k);
+    k++;
+}
+
+
